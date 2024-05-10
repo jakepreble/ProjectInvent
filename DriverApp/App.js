04f9a1da06dc0a050b08be631bc0465383213340
jakepreble <jakepreble3@gmail.com>
2024-05-09 21:40:12 -0400

    uhhhh

--- DriverApp/App.js
+++ DriverApp/App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { useState, Component } from 'react';
+import { View, Text, Button, StyleSheet, SafeAreaView } from 'react-native';
+import { NavigationContainer, DrawerActions } from '@react-navigation/native';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+class app extends Component{
+  
+}
+
+
+
+function Home({ navigation }) {
+  
+  
+  return (
+    <SafeAreaView style={styles.container}>
+    <View>
+      <Text style={styles.title}>Selected Route:</Text>
+      <Text style={styles.selectedRoute}>{"1"}</Text>
+      
+      <Button
+        title="Select Route"
+        onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+        
+        />
+    </View>
+    </SafeAreaView>
+  );
+}
+
+
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Route 1"
+        onPress={() => 
+          
+          props.navigation.dispatch(DrawerActions.closeDrawer())}
+      />
+    </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="Home" component={Home} />
+
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Project Invent Driver Side Application</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex: 0.2,
     justifyContent: 'center',
+    marginHorizontal: 20,
   },
-});
+  title:{
+    textAlign: 'center',
+    fontSize: '24'
+
+  },
+  selectedRoute:{
+    textAlign: 'center',
+    fontSize: '16',
+    marginVertical: 8
+  }
+
+})
